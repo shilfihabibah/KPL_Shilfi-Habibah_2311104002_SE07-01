c0a245bb7f505aa39dd950ad475f34a54a131f79
shilfihabibah <146401507+shilfihabibah@users.noreply.github.com>
2025-02-17 16:19:22 +0700

    Update challenge.js

--- 01_Pengantar_Praktikum_KPL/challenge.js
+++ 01_Pengantar_Praktikum_KPL/challenge.js
@@ -16,7 +16,7 @@ input.question("Siapa nama kamu?", (nama) =>{
     input.question("Jurusan kamu?", (jurusan) =>{
         input.question('Universitas kamu? ', (universitas) => {
             input.question('Umur kamu? ', (umur) => {
-                console.log(`\n--- Data Kamu ---`);
+                console.log(`\n--- Data Pribadi ---`);
                 console.log(`Nama: ${nama}`);
                 console.log(`Jurusan: ${jurusan}`);
                 console.log(`Universitas: ${universitas}`);
