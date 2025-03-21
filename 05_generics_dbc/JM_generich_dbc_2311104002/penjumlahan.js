f90c1c322210fa87269a456426134fc7b16ddb83
shilfi habibah <shilfihabibah127@gmail.com>
2025-03-21 20:54:20 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ 05_generics_dbc/JM_generich_dbc_2311104002/penjumlahan.js
@@ -0,0 +1,24 @@
+class Penjumlahan {
+    static JumlahTigaAngka(a, b, c) {
+        const nimAkhir = 2; 
+        
+        if (nimAkhir === 1 || nimAkhir === 2) {
+            a = parseFloat(a);
+            b = parseFloat(b);
+            c = parseFloat(c);
+        } else {
+            a = Number(a);
+            b = Number(b);
+            c = Number(c);
+        }
+        
+        if (isNaN(a) || isNaN(b) || isNaN(c)) {
+            throw new Error("Semua input harus berupa angka yang valid.");
+        }
+        
+        return a + b + c;
+    }
+}
+
+const hasil = Penjumlahan.JumlahTigaAngka(23, 11, 10);
+console.log("Hasil penjumlahan:", hasil);
