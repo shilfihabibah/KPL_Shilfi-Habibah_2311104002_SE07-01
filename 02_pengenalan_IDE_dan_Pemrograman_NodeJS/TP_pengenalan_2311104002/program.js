4503a5851a209931b58074911ab13b77bb33ab17
shilfi habibah <shilfihabibah127@gmail.com>
2025-03-22 00:50:53 +0700

    first commit

--- 02_pengenalan_IDE_dan_Pemrograman_NodeJS/TP_pengenalan_2311104002/program.js
+++ 02_pengenalan_IDE_dan_Pemrograman_NodeJS/TP_pengenalan_2311104002/program.js
@@ -1,18 +1,9 @@
-
-const readline = require('readline');
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 rl.question("Masukkan satu huruf: ", (huruf) => {
-    if (huruf.length !== 1 || !/[A-Za-z]/.test(huruf)) {
-        console.log("Input harus berupa satu huruf!");
-        rl.close();
-        return;
-    }
-    
     huruf = huruf.toUpperCase(); 
     const vokal = ["A", "I", "U", "E", "O"];
     
@@ -22,7 +13,7 @@ rl.question("Masukkan satu huruf: ", (huruf) => {
         console.log(`Huruf ${huruf} merupakan huruf konsonan`);
     }
 
-    let bilanganGenap = Array.from({ length: 5 }, (_, i) => (i + 1) * 2);
+    let bilanganGenap = [2, 4, 6, 8, 10];
     bilanganGenap.forEach((angka, index) => {
         console.log(`Angka genap ${index + 1} : ${angka}`);
     });
