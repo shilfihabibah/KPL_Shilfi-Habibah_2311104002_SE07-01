ec5b68193dcd039382032b9d7e2c2ef5fc760995
shilfi habibah <shilfihabibah127@gmail.com>
2025-05-25 22:57:11 +0700

    Modul 13

---
+++ 13_Design_Pattern/TP_modul13/main.js
@@ -0,0 +1,18 @@
+import { Subject } from '../Subject.js';
+import { ConcreteObserver } from './Observer.js';
+
+const subject = new Subject();
+
+const observer1 = new ConcreteObserver("Observer A");
+const observer2 = new ConcreteObserver("Observer B");
+
+subject.subscribe(observer1);
+subject.subscribe(observer2);
+
+console.log("Subject mengirim data: Perubahan #1");
+subject.notify("Perubahan #1");
+
+subject.unsubscribe(observer2);
+
+console.log("Subject mengirim data: Perubahan #2");
+subject.notify("Perubahan #2");
